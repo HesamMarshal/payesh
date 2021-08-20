c5c1a9db493ca3eaee1e3226cf3c9e21edd82be6
Hesam Marshal <heak82@hotmail.com>
2021-08-20 19:00:32 +0430

    first push

---
+++ app/src/main/java/ir/hesammarshal/payesh/view/activities/AddUpdateItemActivity.kt
@@ -0,0 +1,13 @@
+package ir.hesammarshal.payesh.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ir.hesammarshal.payesh.R
+
+class AddUpdateItemActivity : AppCompatActivity() {
+    // TODO: Add Heart Logo or image in add screen
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_update_item)
+    }
+}
---
+++ app/src/main/java/ir/hesammarshal/payesh/view/fragments/AllItemsFragment.kt
@@ -0,0 +1,65 @@
+package ir.hesammarshal.payesh.view.fragments
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.*
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import ir.hesammarshal.payesh.R
+import ir.hesammarshal.payesh.databinding.FragmentAllItemsBinding
+import ir.hesammarshal.payesh.view.activities.AddUpdateItemActivity
+import ir.hesammarshal.payesh.viewmodel.HomeViewModel
+
+class AllItemsFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+    private var _binding: FragmentAllItemsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setHasOptionsMenu(true)
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentAllItemsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_all_items, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_add_item -> {
+                startActivity(Intent(requireActivity(),AddUpdateItemActivity::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
