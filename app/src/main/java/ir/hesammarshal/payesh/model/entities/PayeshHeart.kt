6313247fdd96f333df5a55d3212552f39db48206
Hesam Marshal <heak82@hotmail.com>
2021-08-26 13:38:18 +0430

    small modification to class

--- app/src/main/java/ir/hesammarshal/payesh/model/entities/PayeshHeart.kt
+++ app/src/main/java/ir/hesammarshal/payesh/model/entities/PayeshHeart.kt
@@ -4,12 +4,12 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
-data class HeartItems (
+@Entity (tableName = "heart_table")
+data class PayeshHeart (
     @ColumnInfo(name = "heart_date") val heartDate: String,
     @ColumnInfo(name = "heart_time") val heartTime: String,
     @ColumnInfo val systolic: String,
     @ColumnInfo val diastolic: String,
     @ColumnInfo val pulse: String,
     @PrimaryKey(autoGenerate = true) val id: Int = 0
-)
+ )
