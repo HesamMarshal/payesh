d1addeb206be622e7a41b94f23e77118d8eb1599
Hesam Marshal <heak82@hotmail.com>
2021-08-26 13:45:42 +0430

    database.PayeshRoomDatabese Created

---
+++ app/src/main/java/ir/hesammarshal/payesh/model/database/PayeshRoomDatabase.kt
@@ -0,0 +1,32 @@
+package ir.hesammarshal.payesh.model.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import ir.hesammarshal.payesh.model.entities.PayeshHeart
+
+@Database(entities = [PayeshHeart::class], version = 1)
+abstract class PayeshRoomDatabase: RoomDatabase() {
+    companion object{
+        @Volatile
+        private var INSTANCE: PayeshRoomDatabase? = null
+        fun getDatabase(context: Context): PayeshRoomDatabase {
+
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    PayeshRoomDatabase::class.java,
+                    "fav_dish_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+}
