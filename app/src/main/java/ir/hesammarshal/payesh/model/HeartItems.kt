714a29e9f5f57aa956de1011dcc8392895dc9f94
Hesam Marshal <heak82@hotmail.com>
2021-08-26 12:14:46 +0430

    add room to build.gradle - heartItem.kt

---
+++ app/src/main/java/ir/hesammarshal/payesh/model/HeartItems.kt
@@ -0,0 +1,16 @@
+package ir.hesammarshal.payesh.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class HeartItems (
+
+    @ColumnInfo(name = "heart_date") val heartDate: String,
+    @ColumnInfo(name = "heart_time") val heartTime: String,
+    @ColumnInfo val systolic: String,
+    @ColumnInfo val diastolic: String,
+    @ColumnInfo val pulse: String,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0
+)
